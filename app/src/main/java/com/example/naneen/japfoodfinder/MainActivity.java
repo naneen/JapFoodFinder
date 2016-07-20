8711db0357a4c8214bbe0c47e83319f86f240a00
naneen <neen_tta@hotmail.com>
2016-07-20 19:06:39 +0900

    could pick recently capture image

--- app/src/main/java/com/example/naneen/japfoodfinder/MainActivity.java
+++ app/src/main/java/com/example/naneen/japfoodfinder/MainActivity.java
@@ -1,31 +1,15 @@
 package com.example.naneen.japfoodfinder;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -35,13 +19,6 @@ public class MainActivity extends AppCompatActivity {
     private ImageView capturedImage;
     private static final int ACTION_CAMERA = 0;
     private static final int ACTION_LOAD_GALLERY = 1;
-
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,24 +62,9 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, ACTION_LOAD_GALLERY);
     }
 
-    // open camera
     private void openCamera() {
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, ACTION_CAMERA);
-    }
-
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
     }
 
 //    @Override
@@ -111,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
 //        getMenuInflater().inflate(R.menu.menu_main, menu);
 //        return true;
 //    }
-
 //    @Override
 //    public boolean onOptionsItemSelected(MenuItem item) {
 //        // Handle action bar item clicks here. The action bar will
@@ -133,23 +94,26 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             if(requestCode == ACTION_CAMERA && resultCode == RESULT_OK && null!=data) {
-                Bitmap bp = (Bitmap) data.getExtras().get("data");
-                capturedImage.setImageBitmap(bp);
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String filePath = cursor.getString(columnIndex);
+
+                capturedImage.setImageBitmap(BitmapFactory.decodeFile(filePath));
             }
             else if(requestCode == ACTION_LOAD_GALLERY && resultCode == RESULT_OK && null!=data) {
                 Uri selectedImage = data.getData();
                 String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
-                // Get the cursor
                 Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-                // Move to first row
                 cursor.moveToFirst();
-
                 int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
                 String filePath = cursor.getString(columnIndex);
                 cursor.close();
-                // Set the Image in ImageView after decoding the String
+
                 capturedImage.setImageBitmap(BitmapFactory.decodeFile(filePath));
             }
             else {
